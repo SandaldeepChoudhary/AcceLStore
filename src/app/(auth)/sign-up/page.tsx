867aa5bf15e07c9359196808934be5149f8c08b7
Sandaldeep <128312710+SandaldeepChoudhary@users.noreply.github.com>
2024-02-21 17:24:16 +0530

    Initial Commit

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,60 @@
+"use client";
+import { Icons } from "@/components/Icons";
+import { buttonVariants } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { ArrowRight } from "lucide-react";
+import Link from "next/link";
+
+const page = () => {
+  return (
+    <>
+      <div className="container relative flex pt-20 flex-col items-center justify-center lg:px-0">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col items-center space-y-2 text-center">
+            <Icons.logo className="h-20 w-20" />
+            <h1 className="2xl font-bold">Create an account</h1>
+
+            <Link
+              className={buttonVariants({
+                variant: "link",
+                className: "gap-1.5",
+              })}
+              href="/sign-in"
+            >
+              Already have an account? Sign-in{" "}
+              <ArrowRight className="h-4 w-4" />
+            </Link>
+          </div>
+          <div className="grid gap-6">
+            <form>
+              <div className="grid gap-2">
+                <div className="grid gap-2 py-2">
+                  <Label htmlFor="email">Email</Label>
+                  <Input
+                    className={cn({
+                      'focus-visible:ring-red-500': true,
+                    })}
+                    placeholder="you@example.com"
+                  />
+                </div>
+                <div className="grid gap-2 py-2">
+                  <Label htmlFor="password">Password</Label>
+                  <Input
+                    className={cn({
+                      "focus-visible:ring-red-500": true,
+                    })}
+                    placeholder="Password"
+                  />
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default page;
