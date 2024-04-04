a51995a4724958cafb3c58cfc53f4244e4b7bccf
Sandaldeep <sandaldeepkashyap@gmail.com>
2024-04-04 15:01:50 +0530

    Orders Added

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,6 +7,8 @@ import { Users } from "./collections/Users";
 import  dotenv  from "dotenv";
 import { Products } from "./collections/Products/Products";
 import { Media } from "./collections/Media";
+import { ProductFiles } from "./collections/ProductFiles";
+import { Orders } from "./collections/Orders";
 
 dotenv.config({
     path: path.resolve(__dirname, "../.env"),
@@ -14,7 +16,8 @@ dotenv.config({
 
 export default buildConfig({
     serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-    collections: [Users, Products, Media],
+    //!Do not forget to run generate:types before adding a new collection or updating an existing collection
+    collections: [Users, Products, ProductFiles, Media, Orders],
     routes: {
         admin: '/sell'
     },
