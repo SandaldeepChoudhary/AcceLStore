4f84bf58816effa1529a2f81e6ceac6324337a6e
Sandaldeep <sandaldeepkashyap@gmail.com>
2024-04-28 21:21:08 +0530

    Icon Fixed

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,5 +1,7 @@
+import { LucideProps } from "lucide-react";
+
 export const Icons = {
-  logo: () => (
+  logo: (props: LucideProps) => (
     <h2 className="text-lg font-extrabold ">AcceLStore.</h2>
 
 
