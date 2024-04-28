9a24eaec50e4d8b9287ef1386c4e178fb6b65fdd
Sandaldeep <sandaldeepkashyap@gmail.com>
2024-04-28 16:05:54 +0530

    MobileNav Added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,7 @@ import Cart from "./ui/Cart";
 import { getServerSideUser } from "@/lib/payload-utils";
 import {cookies} from "next/headers"
 import UserAccountNav from "./UserAccountNav";
+import MobileNav from "./MobileNav";
 
 const Navbar = async() => {
   const nextCookies = cookies()
@@ -15,15 +16,19 @@ const Navbar = async() => {
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
+        
         <MaxWidthWrapper>
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
               {/* ToDO: Mobile nav */}
-
+              
               <div className="ml-4 flex lg:ml-0">
                 <Link href="/">
                   <Icons.logo className="h-10 w-10"></Icons.logo>
                 </Link>
+              </div>
+              <div className="absolute right-3">
+              <MobileNav />
               </div>
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
