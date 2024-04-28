5b3f6d9027bf278a4240cbf98237a211e318e455
Sandaldeep <sandaldeepkashyap@gmail.com>
2024-04-28 20:21:34 +0530

    Production fixes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,30 +5,29 @@ import NavItems from "./NavItems";
 import { buttonVariants } from "./ui/button";
 import Cart from "./ui/Cart";
 import { getServerSideUser } from "@/lib/payload-utils";
-import {cookies} from "next/headers"
+import { cookies } from "next/headers";
 import UserAccountNav from "./UserAccountNav";
 import MobileNav from "./MobileNav";
 
-const Navbar = async() => {
-  const nextCookies = cookies()
-  const {user} = await getServerSideUser(nextCookies)
+const Navbar = async () => {
+  const nextCookies = cookies();
+  const { user } = await getServerSideUser(nextCookies);
 
   return (
     <div className="bg-white sticky z-50 top-0 inset-x-0 h-16">
       <header className="relative bg-white">
-        
         <MaxWidthWrapper>
           <div className="border-b border-gray-200">
             <div className="flex h-16 items-center">
               {/* ToDO: Mobile nav */}
-              
+
               <div className="ml-4 flex lg:ml-0">
                 <Link href="/">
                   <Icons.logo className="h-10 w-10"></Icons.logo>
                 </Link>
               </div>
               <div className="absolute right-3">
-              <MobileNav />
+                <MobileNav />
               </div>
               <div className="hidden z-50 lg:ml-8 lg:block lg:self-stretch">
                 <NavItems />
@@ -46,19 +45,38 @@ const Navbar = async() => {
                     </Link>
                   )}
 
-                  {user ? null: (<span className="h-6 w-px bg-gray-200" aria-hidden='true'/>)}
-                  {user ? (<UserAccountNav user={user}/>): (<Link href="/sign-up" className={buttonVariants({
-                    variant: 'ghost',
-                  })}>Create account</Link>
+                  {user ? null : (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  )}
+                  {user ? (
+                    <UserAccountNav user={user} />
+                  ) : (
+                    <Link
+                      href="/sign-up"
+                      className={buttonVariants({
+                        variant: "ghost",
+                      })}
+                    >
+                      Create account
+                    </Link>
                   )}
 
-                  {user ? (<span className="h-6 w-px bg-gray-200" aria-hidden='true'/>): null}
+                  {user ? (
+                    <span className="h-6 w-px bg-gray-200" aria-hidden="true" />
+                  ) : null}
 
-                  {user ? null: (<div className="flex lg:ml-6">
-                  <span className="h-6 w-px bg-gray-200" aria-hidden='true'/>
-                    </div>)}
+                  {user ? null : (
+                    <div className="flex lg:ml-6">
+                      <span
+                        className="h-6 w-px bg-gray-200"
+                        aria-hidden="true"
+                      />
+                    </div>
+                  )}
 
-                    <div className="ml-4 flow-root lg:ml-6"><Cart/></div>
+                  <div className="ml-4 flow-root lg:ml-6">
+                    <Cart />
+                  </div>
                 </div>
               </div>
             </div>
