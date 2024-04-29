d39a225084e95d383a1fc37c006999f1d40ba18c
Sandaldeep <sandaldeepkashyap@gmail.com>
2024-04-30 00:32:59 +0530

    Currency Updated

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -9,11 +9,11 @@ export function cn(...inputs: ClassValue[]) {
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: 'USD' | 'EUR' | 'GBP' | 'BDT'
+    currency?: 'USD' | 'INR' |'EUR' | 'GBP' | 'BDT'
     notation?: Intl.NumberFormatOptions['notation']
   } = {}
 ) {
-  const { currency = 'USD', notation = 'compact' } = options
+  const { currency = 'INR', notation = 'compact' } = options
 
   const numericPrice =
     typeof price === 'string' ? parseFloat(price) : price
